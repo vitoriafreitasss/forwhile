878511de4e78a95f5a4d3a57f06b24334ff1209e
Vitória Freitas <vitoriafreitasprog@gmail.com>
2025-05-30 16:59:12 -0300

    Add files via upload

---
+++ at7.py
@@ -0,0 +1,9 @@
+senha = input("digite sua senha")
+senha_correta = "1234"
+while senha != senha_correta:
+   print("tente novamente")
+   senha = input()
+
+print("Senha correta")
+
+#while enquanto  != diferente
